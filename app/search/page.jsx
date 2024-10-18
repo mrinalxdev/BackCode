5a74fe40174dc0fa33cab2c118aadf4364c34b3d
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-10-19 05:19:47 +0530

    v1.0 fixed : csr suspense bugs

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,35 +1,47 @@
-"use client"
+"use client";
 
-import { useState, useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
-import Post from '@/components/Post';
-import Navbar from '@/components/Navbar';
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import Post from "@/components/Post";
+import Navbar from "@/components/Navbar";
+import { Suspense } from "react";
 
 export default function SearchResults() {
   const [results, setResults] = useState([]);
   const searchParams = useSearchParams();
-  const q = searchParams.get('q');
+  const q = searchParams.get("q");
 
   useEffect(() => {
     if (q) {
       fetch(`/api/search?q=${encodeURIComponent(q)}`)
-        .then(response => response.json())
-        .then(data => setResults(data));
+        .then((response) => response.json())
+        .then((data) => setResults(data));
     }
   }, [q]);
 
   return (
-    <div className="container mx-auto">
-      <Navbar />
-      <h1 className="text-3xl font-bold my-8">Search Results for "{q}"</h1>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-5">
-        {results.map((item) => (
-          <Post key={item.id} title={item.title} languages={item.languages} desc={item.desc} link={item.link} />
-        ))}
+    <Suspense>
+      <div className="container mx-auto">
+        <Navbar />
+        <h1 className="text-3xl font-bold my-8">Search Results for "{q}"</h1>
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-5">
+          {results.map((item) => (
+            <Post
+              key={item.id}
+              title={item.title}
+              languages={item.languages}
+              desc={item.desc}
+              link={item.link}
+            />
+          ))}
+        </div>
+        {results.length === 0 && (
+          <div className="h-screen">
+            We are working hard to bring many head starts for your backend
+            projects !!
+          </div>
+        )}
       </div>
-      {results.length === 0 && <div className='h-screen'>
-        We are working hard to bring many head starts for your backend projects !!
-        </div>}
-    </div>
+    </Suspense>
   );
 }
